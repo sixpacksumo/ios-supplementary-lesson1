10cef540a3bd7b13763fcc60b2be651ca216831e
sakko <saklism@gmail.com>
2013-12-04 21:54:39 +0700

    initial

---
+++ Logging/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  Logging
+//
+//  Created by SaKKo on 12/4/13.
+//  Copyright (c) 2013 SaKKo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Logging/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Logging
+//
+//  Created by SaKKo on 12/4/13.
+//  Copyright (c) 2013 SaKKo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKAppDelegate class]));
+    }
+}
